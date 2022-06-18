66fe85e47a37064228e02b023eb3f2c966d3de70
Nivth <kevinzevalja@gmail.com>
2022-06-18 21:04:27 +0700

    first commit

---
+++ src/components/questions_data/data.js
@@ -0,0 +1,39 @@
+export default [
+  {
+    image:
+      "https://awsimages.detik.net.id/content/2012/07/11/10/jokowimetald.jpg",
+    question: "Jokowi adalah",
+    answers: [
+      "Dewan Presiden Indonesia",
+      "Presiden Indonesia",
+      "Presiden Republik Indonesia",
+      "Maling pangsit",
+    ],
+    correct: 4,
+  },
+  {
+    image:
+      "https://asset.kompas.com/crops/x5XMZVkKLDYLrSskv0B7xfTB7EE=/155x0:1000x563/750x500/data/photo/2021/04/12/60741caa65ee2.jpg",
+    question: "Impian dia untuk menyatukan nusantara, siapa dia?",
+    answers: ["Acumalaka","Gajah mada", "Savesta", "Kumalala",],
+    correct: 2,
+  },
+  {
+    question:
+      "Ganti kodenya....",
+    answers: ["Gak", "Shap", "Malas", "Boleh"],
+    correct: 1,
+  },
+  {
+    question: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    answers: ["a", "b", "c", "d"],
+    correct: 1,
+  },
+  {
+    question:
+      "_____",
+    answers: ["?", "!?", "...", "???"],
+    correct: 3,
+  },
+
+];
